25bfea4c802105757810c05bdeab56d216236b79
Cihan Selim <chnselim@outlook.com>
2016-04-10 18:44:34 +0300

    updated

--- backgroundListener.py
+++ backgroundListener.py
@@ -1,39 +1,74 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import speech_recognition as sr
-import ScrolledText
-from Tkinter import *
 from commands import *
 
+lst = [(u"kontrol", ifStatement),
+       (u"alta geç", newLine),
+       (u"küçüktür", lessThan),
+       (u"büyük eşittir", bigEqual),
+       (u"büyüktür", greaterThan),
+       (u"küçük eşittir", lowEqual),
+       (u"boşluk", getSpace),
+       (u"yazdır", getPrint),
+       (u"rastgele sayı", randomNum),
+       (u"paragraf", getTab)]
+
 listen = True
+
+def callFSetI(indx, i):
+    global lst
+    lst[indx][1]()
+    return (i+1)
+
 def callback(recognizer, audio):
-    global listen
+    global listen, lst
     try:
         if listen:
-            list = [u"kontrol",u"alta geç",u"rastgele sayı",u"küçüktür",u"boşluk",u"yazdır"]
-
-            if recognizer.recognize_google(audio, language="tr") not in list:
-                textPad.insert(INSERT, recognizer.recognize_google(audio, language="tr"))
-
-            else:
-                if recognizer.recognize_google(audio, language="tr") == u"kontrol":
-                     ifStatement()
-
-                elif recognizer.recognize_google(audio, language="tr") == u"rastgele sayı":
-                     randomNum()
-
-                elif recognizer.recognize_google(audio, language="tr") == u"yazdır":
-                     printL(recognizer, audio)
-
-                elif recognizer.recognize_google(audio, language="tr") == u"alta geç":
-                     newLine()
-
-                elif recognizer.recognize_google(audio, language="tr") == u"küçüktür":
-                     lowerThan()
-
-                elif recognizer.recognize_google(audio,language="tr") == u"boşluk":
-                     space()
+            # a = recognizer.recognize_google(audio, language="tr")
+            a = recognizer.recognize_google(audio, language="tr")
+            words = a.lower().split()
+            lstt = [s for s, f in lst]
+            i = 0
+            while i < len(words):
+                if words[i] not in lstt:
+                    if (words[i] == lst[1][0].split()[0]):
+                        if (len(words) > i + 1) and (words[i+1] == lst[1][0].split()[1]):
+                            i = callFSetI(1, i)
+                        else:
+                            standard_write(words[i])
+                    elif (words[i] == lst[3][0].split()[0]):
+                        if (len(words) > i + 1) and (words[i+1] == lst[3][0].split()[1]):
+                            i = callFSetI(3, i)
+                        else:
+                            standard_write(words[i])
+                    elif (words[i] == lst[5][0].split()[0]):
+                        if (len(words) > i + 1) and (words[i+1] == lst[5][0].split()[1]):
+                            i = callFSetI(5, i)
+                        else:
+                            standard_write(words[i])
+                    elif (words[i] == lst[8][0].split()[0]):
+                        if (len(words) > i + 1) and (words[i+1] == lst[8][0].split()[1]):
+                            i = callFSetI(8, i)
+                        else:
+                            standard_write(words[i])
+                    else:
+                        standard_write(words[i])
+                else:
+                    # print words[i]
+                    if words[i] == lst[0][0]:
+                        lst[0][1]()
+                    elif words[i] == lst[2][0]:
+                        lst[2][1]()
+                    elif words[i] == lst[4][0]:
+                        lst[4][1]()
+                    elif words[i] == lst[6][0]:
+                        lst[6][1]()
+                    elif words[i] == lst[7][0]:
+                        lst[7][1]()
+                    elif words[i] == lst[9][0]:
+                        lst[9][1]()
+                i += 1
         else:
             listen = False
     except sr.UnknownValueError:
@@ -42,7 +77,7 @@ def callback(recognizer, audio):
         print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
 r = sr.Recognizer()
-r.energy_threshold = 800
+r.energy_threshold = 4000
 r.pause_threshold = 0.5
 m = sr.Microphone()
 
@@ -51,30 +86,31 @@ with m as source:
 
 stop_listening = r.listen_in_background(m, callback)
 
-#Tkinter starts here
 
-#Editor menus here
-menu = Menu(root)
-root.config(menu=menu)
-filemenu = Menu(menu)
-menu.add_cascade(label="File", menu=filemenu)
-# filemenu.add_command(label="New")
-filemenu.add_command(label="Open...", command=open_command)
-filemenu.add_command(label="Save", command=save_command)
-filemenu.add_separator()
-filemenu.add_command(label="Exit", command=exit_command)
-helpmenu = Menu(menu)
-menu.add_cascade(label="Help", menu=helpmenu)
-helpmenu.add_command(label="About...", command=about_command)
-textPad.pack()
-root.mainloop()
-
-root.resizable(False,True) #editor enine buyumez, boyuna buyur
-root.wm_protocol("WM_DELETE_WINDOW", Tk) #kapatmayi engeller
+# Tkinter starts here
 textPad.quit()
-root.wm_attributes("-alpha", 0.9) #opacity
 textPad.bind('<F12>', stopKey)
 textPad.bind('<F11>', startKey)
+
+# Modifications on editor
+root.wm_attributes("-alpha", 0.8)  # opacity
+root.resizable(False, True)  # editor enine buyumez, boyuna buyur
+root.wm_protocol("WM_DELETE_WINDOW", Tk)  # kapatmayi engeller
+
+# Editor menus here
+menu = Menu(root)
+root.config(menu=menu)
+file_menu = Menu(menu)
+menu.add_cascade(label="File", menu=file_menu)
+file_menu.add_command(label="Open...", command=open_command)
+file_menu.add_command(label="Save", command=save_command)
+file_menu.add_separator()
+file_menu.add_command(label="Exit", command=exit_command)
+help_menu = Menu(menu)
+menu.add_cascade(label="Help", menu=help_menu)
+help_menu.add_command(label="About...", command=about_command)
+textPad.pack()
+root.mainloop()
 
 import time
 for _ in range(50):
